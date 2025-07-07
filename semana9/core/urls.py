0f570ad0d0ecd3c3c25861361180b1bec32667aa
Nicolas-FelSi <nicolasfelipe3702@gmail.com>
2025-07-07 17:30:27 -0300

    começando a fazer avaliação dos textos

--- semana9/core/urls.py
+++ semana9/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, livro, cadastrar_livro, del_livro, edit_livro
+from .views import index, livro, cadastrar_livro, del_livro, edit_livro, avaliar_textos
 
 urlpatterns = [
     path('', index),
     path('Livro/<int:pk>', livro, name='Livro'),
     path('del_livro/<int:pk>', del_livro, name='deletar_livro_url'),
     path('edit_livro/<int:pk>', edit_livro, name='editar_livro_url'),
-    path('cad_livro', cadastrar_livro, name='cadastrar_livro_url')
+    path('cad_livro', cadastrar_livro, name='cadastrar_livro_url'),
+    path('avaliar_textos', avaliar_textos, name='avaliar_textos_url'),
 ]
