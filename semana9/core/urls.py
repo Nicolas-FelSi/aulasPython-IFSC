159b29de218264b95e057e2b43f0a8af664c3754
Nicolas-FelSi <nicolasfelipe3702@gmail.com>
2025-07-11 15:52:04 -0300

    login e cadastro

--- semana9/core/urls.py
+++ semana9/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, livro, cadastrar_livro, del_livro, edit_livro, avaliar_textos
+from .views import index, livro, cadastrar_livro, del_livro, edit_livro, cad_user, entrar, sair
 
 urlpatterns = [
     path('', index),
@@ -7,5 +7,7 @@ urlpatterns = [
     path('del_livro/<int:pk>', del_livro, name='deletar_livro_url'),
     path('edit_livro/<int:pk>', edit_livro, name='editar_livro_url'),
     path('cad_livro', cadastrar_livro, name='cadastrar_livro_url'),
-    path('avaliar_textos', avaliar_textos, name='avaliar_textos_url'),
+    path('cad_user', cad_user, name='cad_user_url'),
+    path('entrar', entrar, name='entrar_url'),
+    path('sair', sair, name='sair_url')
 ]
--- semana9/core/views.py
+++ semana9/core/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import Livro
+from django.contrib.auth.models import User
+from django.http.response import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url="/entrar")
 def index(request):
     livros = Livro.objects.all()
     context = {
@@ -45,18 +50,49 @@ def edit_livro(request, pk):
         return redirect('/')
     
 def cadastrar_livro(request):
-    
-    if request.method == 'GET':
-        return render(request, 'cad_livro.html')
-    elif request.method == 'POST':
-        titulo = request.POST.get('titulo')
-        preco = request.POST.get('preco')
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            return render(request, 'cad_livro.html')
+        elif request.method == 'POST':
+            titulo = request.POST.get('titulo')
+            preco = request.POST.get('preco')
 
-        livro = Livro(
-            title = titulo,
-            price = preco,
-        )
+            livro = Livro(
+                title = titulo,
+                price = preco,
+            )
 
-        livro.save()
-        return redirect('/')
+            livro.save()
+            return redirect('/')
+    else:
+        return redirect("/entrar")
     
+def cad_user(request):
+    if request.method == "GET":
+        return render(request, 'cad_user.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.create_user(username, email, password)
+        user.save()
+        return redirect("/entrar")
+
+def entrar(request):
+    if request.method == "GET":
+        return render(request, "entrar.html")
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect("/")
+        else:
+            return HttpResponse("Falha ao tentar autenticar")
+        
+def sair(request):
+    logout(request)
+    return redirect("/entrar")
